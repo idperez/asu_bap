ec3c3327d022ec14611235ea9eeff3becbb5878b
isidro <idperez@asu.edu>
2016-06-18 22:30:48 -0400

    start of headers

---
+++ html/headers/photo-header.php
@@ -0,0 +1,62 @@
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       
+        <title>Beta Alpha Psi</title>
+
+        <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+        
+        <link rel="stylesheet" href="/bap/css/animate.css">
+        <link rel="stylesheet" href="/bap/css/icomoon.css">
+        <link rel="stylesheet" href="/bap/css/bootstrap.css">
+        <link rel="stylesheet" href="/bap/css/superfish.css">
+        <link rel="stylesheet" href="/bap/css/style.css">
+        
+        <script src="/bap/js/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <div id="fh5co-wrapper">
+        <div id="fh5co-page">
+        <div id="fh5co-header">
+            <header id="fh5co-header-section">
+                <div class="container">
+                    <div class="nav-header">
+                        <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+                        <h1 id="fh5co-logo"><a href="/bap/html/index.html">Beta Alpha Psi</a></h1>
+                        <nav id="fh5co-menu-wrap" role="navigation">
+                            <ul class="sf-menu" id="fh5co-primary-menu">
+                                <li>
+                                    <a href="/bap/html/index.html">Home</a>
+                            </li>
+                                <li>
+                                    <a href="portfolio.html">Portfolio</a>
+                                </li>
+                                <li>
+                                    <a href="services.html" class="fh5co-sub-ddown">Services</a>
+                                     <ul class="fh5co-sub-menu">
+                                        <li><a href="left-sidebar.html">Web Development</a></li>
+                                        <li><a href="right-sidebar.html">Branding &amp; Identity</a></li>                                    
+                                        <li><a href="#">UI Animation</a></li>
+                                        <li><a href="#">Copywriting</a></li>
+                                        <li><a href="#">Photography</a></li> 
+                                    </ul>
+                                </li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                               
+                                <li class="active">
+                                    <a href="/bap/php/login.php">Login</a>
+                                </li>
+                            </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+        </div>
+        </div>
+        
+    </body>
+</html>
