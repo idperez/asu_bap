617ba31480e889f6b622a944699e557970981378
Drew Demechko <drew_demechko@yahoo.com>
2016-06-20 20:43:49 -0400

    Added AddMember functions, one for short and long. Example of a short entry is an admin setting up an account but not knowing every field of info, leaving that for the user to specify. Updated constructor to take in new sql fields.

--- php/member/MemberList.php
+++ php/member/MemberList.php
@@ -9,10 +9,14 @@ class MemberList
     private $databaseService;
     private $head;
     private $tail;
+    private $tableName;
     
-    function MemberList()
+    function MemberList($tableName)
     {
-           $this->databaseService = new DatabaseService;
+        //reduce the amount of hardcoding for queries
+        $this->tableName = $tableName;
+        
+        $this->databaseService = new DatabaseService;
            $members = $this->databaseService->performQuery("
            SELECT * FROM member
            ");
@@ -21,17 +25,54 @@ class MemberList
            while ($row = mysql_fetch_assoc($members)) 
            {
                //param 'fieldname' the way it appears in the sql database
-               //??todo?? - consider updating title to jobtitle in database
-               //??todo?? - consider matching sql field names to match php code or vice versa
                $tempMember = new Member($row['id'], $row['firstname'], $row['lastname'], $row['email'],
                     $row['title'], $row['linkedin'], $row['phone'], $row['picture_path'], $row['graduated'], 
-                    $row['gradYear'], $row['major1'], $row['major2'], $row['major3'], $row['password'], $row['level']
+                    $row['gradYear'], $row['major1'], $row['major2'], $row['major3'], $row['password'], 
+                    $row['level'], $row['state'], $row['city'], $row['bio']
                );
-                echo $tempMember->getJobTitle();
-                echo " ";
+               //todo - add $tempMember to MembersList
+               $size++;
            }
            
            $this->databaseService->freeResource($members);
-    }    
+    }
+    
+    function addMemberPartial($tableName, $firstName, $lastName, $email, $password, $level)
+    {
+        $tempMember = $this->databaseService->performQuery("
+        INSERT INTO ".$tableName." (firstname, lastname, email, password, level) 
+        VALUES (".$firstName.",".$lastName.",".$email.",".$password.",".$level.")
+        ");
+        
+        //todo - create new Member object based on $tempMember['id']
+        //todo - append Member object to end of MembersList
+        $size++;
+        
+        //free resource
+        $this->databaseService->freeResource($tempMember);
+    }
+    
+    function addMemberFull($firstName, $lastName, $email, $jobTitle, $linkedInUrl, $phoneNumber, $imagePath, 
+        $hasGraduated, $graduationYear, $major, $major2, $major3, $password, $level, $state, $city, $bio)
+    {
+        $tempMember = $this->databaseService->performQuery("
+        INSERT INTO ".$tableName." (firstname, lastname, email, title, linkedin, phone, picture_path, 
+        graduated, gradYear, major1, major2, major3, password, level, state, city, bio) 
+        VALUES (".$firstName.",".$lastName.",".$email.",".$jobTitle.",".$linkedInUrl.",".$phoneNumber.
+        ",".$imagePath.",".$hasGraduated.",".$graduationYear.",".$major.",".$major2.",".$major3.",".
+        $password.",".$level.",".$state.",".$city.",".$bio.")
+        ");
+        
+        //todo - create new Member object based on $tempMember['id']
+        //todo - append Member object to end of MembersList
+        $size++;
+        
+        //free resource
+        $this->databaseService->freeResource($tempMember);
+    }
+    
+    function deleteMember($id)
+    {
+        
+    }     
 }
-?>
