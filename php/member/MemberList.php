8e1376358a68deca7d039a436af5968f3dba49f6
Drew Demechko <drew_demechko@yahoo.com>
2016-06-20 21:12:15 -0400

    Added Officer as dependency. Added todos.

--- php/member/MemberList.php
+++ php/member/MemberList.php
@@ -2,6 +2,7 @@
 $root = realpath($_SERVER["DOCUMENT_ROOT"]); 
 include $root.'/bap/php/databaseservice.php';
 include $root.'/bap/php/member/Member.php';
+include $root.'bap/php/member/Officer.php';
 
 class MemberList 
 {
@@ -17,24 +18,27 @@ class MemberList
         $this->tableName = $tableName;
         
         $this->databaseService = new DatabaseService;
-           $members = $this->databaseService->performQuery("
-           SELECT * FROM member
-           ");
+        
+        $members = $this->databaseService->performQuery("
+        SELECT * FROM member
+        ");
            
-           //create new members and append to list
-           while ($row = mysql_fetch_assoc($members)) 
-           {
-               //param 'fieldname' the way it appears in the sql database
-               $tempMember = new Member($row['id'], $row['firstname'], $row['lastname'], $row['email'],
-                    $row['title'], $row['linkedin'], $row['phone'], $row['picture_path'], $row['graduated'], 
-                    $row['gradYear'], $row['major1'], $row['major2'], $row['major3'], $row['password'], 
-                    $row['level'], $row['state'], $row['city'], $row['bio']
-               );
-               //todo - add $tempMember to MembersList
-               $size++;
-           }
-           
-           $this->databaseService->freeResource($members);
+        //create new members and append to list
+        while ($row = mysql_fetch_assoc($members)) 
+        {
+            //param 'fieldname' the way it appears in the sql database
+            $tempMember = new Member($row['id'], $row['firstname'], $row['lastname'], $row['email'],
+            $row['title'], $row['linkedin'], $row['phone'], $row['picture_path'], $row['graduated'], 
+            $row['gradYear'], $row['major1'], $row['major2'], $row['major3'], $row['password'], 
+            $row['level'], $row['state'], $row['city'], $row['bio']
+            );
+            
+            //todo - add $tempMember to MembersList
+            $size++;
+        }
+        
+        //free resources   
+        $this->databaseService->freeResource($members);
     }
     
     function addMemberPartial($tableName, $firstName, $lastName, $email, $password, $level)
@@ -73,6 +77,7 @@ class MemberList
     
     function deleteMember($id)
     {
-        
+        //using a LinkedList is perfect because there should not be too many deletions other than for testing purposes
+        //todo - traverse through MembersList and delete where the id matches.
     }     
 }
