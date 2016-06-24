baec92a0f0f33ea0dda14e0ed08d18c1e7a963fd
Drew Demechko <drew_demechko@yahoo.com>
2016-06-24 11:15:13 -0400

    Added CakePHP framework on top of existing project. Will delete prior project when all data/files have been converted.

---
+++ Website/app/Controller/MembersController.php
@@ -0,0 +1,19 @@
+<?php
+class Members extends AppController{
+    
+    public function index()
+    {
+        
+    }
+    
+    public function search()
+    {
+        
+    }
+    
+    public function _getId()
+    {
+        
+    }
+}
+?>
