c2ba4c909285d0d42a6f0b59fac5dca607f8a07a
Drew Demechko <drew_demechko@yahoo.com>
2016-06-28 19:35:20 -0400

    Added new db schema.

---
+++ Website/SQL/members.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2016 at 01:34 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `title` varchar(25) NOT NULL,
+  `linkedin` varchar(200) NOT NULL,
+  `phone` varchar(25) NOT NULL,
+  `graduated` tinyint(1) NOT NULL,
+  `graduation_year` varchar(25) NOT NULL,
+  `graduation_semester` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `major_one` varchar(50) NOT NULL,
+  `major_two` varchar(50) NOT NULL,
+  `major_three` varchar(50) NOT NULL,
+  `minor` varchar(50) NOT NULL,
+  `level` varchar(40) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `bio` text NOT NULL,
+  `photo_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`id`, `first_name`, `last_name`, `email`, `title`, `linkedin`, `phone`, `graduated`, `graduation_year`, `graduation_semester`, `password`, `major_one`, `major_two`, `major_three`, `minor`, `level`, `state`, `city`, `bio`, `photo_id`) VALUES
+(1, 'Isidro', 'Perez', 'isidro.perez@live.com', 'admin', 'https://www.linkedin.com/in/isidro-perez-09645196?trk=hp-identity-name', '7608777804', 0, '2016', '', 'test', '0', '', '', '', '', '', '', '', 1),
+(5, 'Jeb', 'Johnson', 'jebby.joe@icloud.com', 'Director of Software', 'https://www.linkedin.com/in/kaitlinfayemitchell?authType=NAME_SEARCH&authToken=8h9U&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CentityType%3AentityHistoryName%2CclickedEntityId%3Amynet', '7608777804', 0, '2017', '', 'jeb', '0', '', '', '', '', '', '', '', 1),
+(7, '', '', 'working@live.com', '', '', '', 0, '', '', '$2y$10$ZTdjZGUxMmI2ZTU2MzYzMungwaJZFWt64jPQZcJcJYUHAuE05rsvS', '0', '', '', '', '', '', '', '', 1),
+(9, 'Doug', 'Carrol', 'dcarrol@gmail.com', '', '', '', 0, '', '', '$2y$10$M2EyZjg2YWFlZTRjNGY1YOB.OEP22JaYXXM5Dnb6QX0YrYnsDHJti', '0', '', '', '', 'Candidate', '', '', '', 1),
+(10, 'Tommy', 'Murphey', 'tmurph@asu.edu', '', '', '', 0, '', '', '$2y$10$ZDQ3MmQzMzVjYmY4MDhhMOpltW2KvxvB2hc1C1xSgDQHmuK8fZdV6', '0', '', '', '', 'Candidate', '', '', '', 1),
+(11, 'Greg', 'Perez', 'gregsocal@gmail.com', 'Manager', '', '(760) 877-7804', 0, '2017', '', '$2y$10$MGY1NzI5ZDhmZGQyYzRhN.WFo6DrEzUMEUdG7lZBA1FomC6Ei1mum', '0', '', '', '', 'Candidate', '', '', '', 1),
+(12, 'Isidro', 'Perez', 'isidro.perez.espn@gmail.com', 'Manager', 'https://linkedin.com', '7608777804', 0, '2017', '', '$2y$10$NzQxZTgyMTY3NzVhNmRkNuYqXH2tXOxR5uWHb8k.3XIqGUPG.kVLS', '1', '', '', '', 'admin', '', '', '', 1),
+(13, 'Drew', 'Demechko', 'isd@isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(14, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(15, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(16, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(17, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(18, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(19, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(20, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(21, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(22, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(23, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(24, 'Dre', 'Demechko', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(25, 'Dre', 'Alan', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(26, 'Dre', 'Alan', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(27, 'Dre', 'Alan', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(28, 'Dre', 'Bri', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(29, 'Dre', 'Bri', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(30, 'Dre', 'Zi', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(31, 'Dre', 'Zi', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(32, 'Dre', 'Li', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(33, 'Dre', 'Li', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(34, 'Dre', 'Li', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(35, 'Dre', 'Mom', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(36, 'Dre', 'Mom', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(37, 'Dre', 'Mom', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(38, 'Dre', 'Yi', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(39, 'Dre', 'Yi', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(40, 'Dre', 'Yi', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(41, 'Dre', 'Yi', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(42, 'Dre', 'Vin', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(43, 'Dre', 'Vin', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(44, 'Dre', 'Vin', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(45, 'Dre', 'Vin', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(46, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(47, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(48, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(49, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(50, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(51, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(52, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(53, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(54, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(55, 'Dre', 'Eer', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(56, 'Dre', 'Fun', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(57, 'Dre', 'Fun', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1),
+(58, 'Dre', 'Fun', 'isd.com', '', '', '', 0, '', '', 'test', '0', '', '', '', 'Officer', '', '', '', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
