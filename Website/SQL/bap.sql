cdd79cc1c27d9f289ba7e93767fb8b353e4ca79c
Isidro Perez <Isidro.D.Perez@espn.com>
2016-08-11 16:29:34 -0400

    added auto increment to prospective member id

--- Website/SQL/bap.sql
+++ Website/SQL/bap.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 11, 2016 at 10:11 PM
+-- Generation Time: Aug 11, 2016 at 10:28 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.5.35
 
@@ -35,6 +35,13 @@ CREATE TABLE `prospectives` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `prospectives`
+--
+
+INSERT INTO `prospectives` (`id`, `first_name`, `last_name`, `email`, `time`) VALUES
+(1, '', 'Perez', 'isidro.perez@live.com', '2016-08-11 22:22:30.000000');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -44,6 +51,15 @@ CREATE TABLE `prospectives` (
 ALTER TABLE `prospectives`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `prospectives`
+--
+ALTER TABLE `prospectives`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
